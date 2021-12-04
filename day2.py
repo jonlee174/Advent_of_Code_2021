f331c3ed20c9a204c8e341b7a29175c62e4b662f
jonlee174 <88959208+jonlee174@users.noreply.github.com>
2021-12-03 22:37:31 -0600

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,28 +1,26 @@
 
 from input import input_file
 
-def part1():
-    horiz = []
-    up = []
-    down = []
-    for i in input_file('AoC#2_input.txt'):
+file = input_file('input_files/day2_input.txt')
+def part1(input_values):
+    horiz = 0
+    up = 0
+    down = 0
+    for i in input_values:
         if 'f' in i:
-            horiz.append(int(i[-1]))
+            horiz += int(i[-1])
         elif 'n' in i:
-            down.append(int(i[-1]))
+            down += int(i[-1])
         elif 'p' in i:
-            up.append(int(i[-1]))
-    horizontal_val = sum(horiz)
-    up_val = sum(up)
-    down_val = sum(down)
-    depth_val = down_val - up_val
-    return horizontal_val*depth_val
+            up += int(i[-1])
+    depth = down - up
+    return horiz*depth
 
-def part2():
+def part2(input_values):
     fd = 0
     aim = 0
     depth = 0
-    for i in input_file('AoC#2_input.txt'):
+    for i in input_values:
         if 'f' in i:
             fd += int(i[-1])
             depth += int(i[-1]) * aim
@@ -33,4 +31,4 @@ def part2():
     return fd*depth
 
 if __name__ == '__main__':
-    print(f'Part 1: {part1()}\nPart 2: {part2()}')
+    print(f'Part 1: {part1(file)}\nPart 2: {part2(file)}')
