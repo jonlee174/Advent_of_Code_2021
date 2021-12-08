4515fe73f28d6a56f9f937374365fc07946c6632
Jonathan Lee <88959208+jonlee174@users.noreply.github.com>
2021-12-07 20:55:59 -0600

    Update day3.py

--- day3.py
+++ day3.py
@@ -1,56 +1,73 @@
 from input import input_file
-from statistics import mode
 
 input_values = input_file('input_files/day3_input.txt')
-lists = [[] for _ in range(13)]
-def part1(file_list):
-    for i in file_list:
-        lists[0].append(i[0])
-        lists[1].append(i[1])
-        lists[2].append(i[2])
-        lists[3].append(i[3])
-        lists[4].append(i[4])
-        lists[5].append(i[5])
-        lists[6].append(i[6])
-        lists[7].append(i[7])
-        lists[8].append(i[8])
-        lists[9].append(i[9])
-        lists[10].append(i[10])
-        lists[11].append(i[11])
+def part_one(nums: list[str]) -> int:
+    gamma_rate = ''
+    epsilon_rate = ''
+    num_length = len(nums[0])
 
-        gamma = str(mode(lists[0]))+str(mode(lists[1]))+str(mode(lists[2]))+str(mode(lists[3]))+str(mode(lists[4]))+str(mode(lists[5]))+str(mode(lists[6]))+str(mode(lists[7]))+str(mode(lists[8]))+str(mode(lists[9]))+str(mode(lists[10]))+str(mode(lists[11]))
-        epsilon = ''
-        for i in gamma:
-            if i == '0':
-                epsilon += '1'
-            elif i == '1':
-                epsilon += '0'
-        gamma = int(gamma, 2)
-        epsilon = int(epsilon, 2)
-    return gamma*epsilon
+    for i in range(num_length):
+        zeros_count = 0
+        ones_count = 0
+        for num in nums:
+            if num[i] == "0":
+                zeros_count += 1
+            elif num[i] == "1":
+                ones_count += 1
+        if zeros_count > ones_count:
+            gamma_rate += "0"
+            epsilon_rate += "1"
+        else:
+            gamma_rate += "1"
+            epsilon_rate += "0"
 
-lists_2 = [[] for _ in range(13)]
-def part2(file_list):
-    oxygen_list = []
-    part1(input_values)
-    for i in file_list:
-        if i[0] == str(mode(lists[0])):
-            oxygen_list.append(i)
-            for o in oxygen_list:
-                lists_2[0].append(o[0])
-                lists_2[1].append(o[1])
-                lists_2[2].append(o[2])
-                lists_2[3].append(o[3])
-                lists_2[4].append(o[4])
-                lists_2[5].append(o[5])
-                lists_2[6].append(o[6])
-                lists_2[7].append(o[7])
-                lists_2[8].append(o[8])
-                lists_2[9].append(o[9])
-                lists_2[10].append(o[10])
-                lists_2[11].append(o[11])
-                if o[0] != str(mode(lists_2[0])):
-                    oxygen_list.remove(o)
-    print(oxygen_list)
-part2(input_values)
-print(part1(input_values))
+    return int(gamma_rate, 2) * int(epsilon_rate, 2)
+
+
+def part_two(nums: list[str]) -> int:
+    o2_list = nums.copy()
+    co2_list = nums.copy()
+    num_length = len(nums[0])
+
+    for i in range(num_length):
+        zeros_count = 0
+        ones_count = 0
+        for num in o2_list:
+            if num[i] == "0":
+                zeros_count += 1
+            elif num[i] == "1":
+                ones_count += 1
+        for num in o2_list.copy():
+            if len(o2_list) > 1:
+                if zeros_count > ones_count and num[i] == "1":
+                    o2_list.remove(num)
+                elif ones_count > zeros_count and num[i] == "0":
+                    o2_list.remove(num)
+                elif ones_count == zeros_count and num[i] == "0":
+                    o2_list.remove(num)
+
+    o2_rating = int(o2_list[0], 2)
+
+    for i in range(num_length):
+        zeros_count = 0
+        ones_count = 0
+        for num in co2_list:
+            if num[i] == "0":
+                zeros_count += 1
+            elif num[i] == "1":
+                ones_count += 1
+        for num in co2_list.copy():
+            if len(co2_list) > 1:
+                if zeros_count > ones_count and num[i] == "0":
+                    co2_list.remove(num)
+                elif ones_count > zeros_count and num[i] == "1":
+                    co2_list.remove(num)
+                elif ones_count == zeros_count and num[i] == "1":
+                    co2_list.remove(num)
+
+    co2_rating = int(co2_list[0], 2)
+
+    return co2_rating * o2_rating
+
+if __name__ == '__main__':
+    print(f'Part 1: {part_one(input_values)}\nPart 2: {part_two(input_values)}')
